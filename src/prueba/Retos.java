95262be848ea3642ac1d662e81fc10fed9448e24
AlumnoMañana <AlumnoMaana@DESKTOP-QDURS72>
2018-10-24 08:22:16 +0100

    Version 1

---
+++ src/prueba/Retos.java
@@ -0,0 +1,37 @@
+package prueba;
+
+public class Retos {
+
+	// ACTIVIDAD : Devolver el número menor de SIETE NÚMEROS
+		public static int calNumMenor(int x, int y, int z, int w) {
+			if (x < y)
+				if (x < z)
+					if (x < w)
+						return x;
+					else
+						return w;
+				else
+					if (z < w)
+						return z;
+				else 
+					return w;
+				else 
+					
+				if (y < z)
+					if (y < w)
+						return y;
+					else 
+						return w;
+				else
+					return z;
+		}
+
+		public static void main(String[] args) {
+			//
+			int a = 12, b = 4, c = 8, d = 20;
+
+			System.out.println("el menor es : " + calNumMenor(a, b, c, d));
+		
+		
+		}	
+}
